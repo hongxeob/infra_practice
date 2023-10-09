497822d7d6eaeba3c2806ec02502cc8e56776d9a
Hongseob Lee <sjun2918@naver.com>
2023-10-09 14:34:27 +0900

    chore: project init

---
+++ src/main/java/com/example/infra_practice/InfraPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.infra_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/infra_practice/InfraPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infra_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
